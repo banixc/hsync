e02bd439e03f1c958f467875a730b6b4f3eed108
banixcyan <banixcyan@tencent.com>
2017-10-27 19:55:47 +0800

    rm no used file

--- hsync/__main__.py
+++ hsync/__main__.py
@@ -1,18 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-
-@author: banixcyan
-
-@license: (C) Copyright 2013-2017, Tencent Corporation Limited.
-
-@contact: banixcyan@gmail.com
-
-@time: 2017/10/27 13:30
-
-@desc:
-
-"""
 from local import main
 
 main()
